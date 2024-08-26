c0b6b8c5bc18732ebfa86d62f848bad823851b97
Nick H <nickbhenderson95@gmail.com>
2024-08-25 19:23:12 -0500

    Just finished setting up Mongo Atlas. This should be the final commit before using R ender to deploy the application.

--- app.js
+++ app.js
@@ -15,12 +15,14 @@ const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
+const MongoStore = require('connect-mongo');
+const dbUrl = process.env.DB_URL;
 
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 const authRoutes = require('./routes/auth');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp');
+mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -39,7 +41,20 @@ app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(mongoSanitize());
 
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    touchAfter: 24 * 60 * 60,
+    crypto: {
+        secret: 'thisshouldbeabettersecret!'
+    }
+});
+
+store.on("error", function(e) {
+    console.log("Store error!", e)
+})
+
 const sessionConfig = {
+    store,
     name: '_ycsid',
     secret: 'thisshouldbebetter',
     resave: false,
